f8c94b3f2a9e79be21095e637f343a3fb1d5b606
Adelina609 <adeliachar@gmail.com>
2019-05-10 23:59:00 +0300

    fix interactor

--- app/src/main/java/com/github/kornilovmikhail/mvvmandroidproject/interactor/TopNewsInteractor.kt
+++ app/src/main/java/com/github/kornilovmikhail/mvvmandroidproject/interactor/TopNewsInteractor.kt
@@ -5,14 +5,12 @@ import com.github.kornilovmikhail.mvvmandroidproject.model.News
 
 class TopNewsInteractor(private val newsRepository: NewsRepository) {
 
-    //TODO
-    suspend fun getTopNews(): List<News> =
-        newsRepository.getTopNews()
-            .map {
-                deleteTopNews()
-                cacheTopNews(it)
-            }
-            .flatMap { newsRepository.getTopNews() }
+    suspend fun getTopNews(): List<News> {
+        val topNews =  newsRepository.getTopNews()
+        deleteTopNews()
+        cacheTopNews(topNews)
+        return topNews
+    }
 
     suspend fun cacheTopNews(newsList: List<News>) =
         newsRepository.cacheTopNews(newsList)
